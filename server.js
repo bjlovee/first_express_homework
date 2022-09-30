e6b12bbfd027bbea7259af638b6c6132caac4996
bjlovee <bj000051@yahoo.com>
2022-09-29 22:15:57 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 // require modules
 const { response } = require('express')
 const express = require('express')
-
+const tip = [
+    {
+        100: '$100'
+    },
+    {
+        20: '20%'
+    }
+ ]
 // Create express app 
 const app = express()
 
@@ -9,14 +16,24 @@ const app = express()
 
 app.get('/greeting', (req, res) => {
 res.send('<h1>Express Is Cool!👨🏾</h1>')
-
 })
 
-app.get('/greeting/:frontedPeople', (req, res) => {
-    res.send('<h1>I Love The Backend☕️ sorry frontend people</h1>')
-    
+app.get('/greeting/:frontend', (req, res) => {
+    res.send('<h1>I Love The Backend☕️ sorry frontend people🙍🏾‍♂️</h1>')
     })
     
+app.get('/tip', (req, res) => {
+res.send('<h1>Hang Tight..</h1>')
+})
+
+app.get('/tip/:total', (req, res) => {
+    res.send('<h1>Your total today is $85.32</h1>')
+    })
+
+    app.get('/tip/:total/:percentage', (req, res) => {
+        res.send('<h1>You have tipped 12%, which brings your total up to $95.55</h1>')
+        })
+
 // End Route
 
 
